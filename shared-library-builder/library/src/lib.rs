00ba77c73248fc044cf2d0546ca40ba02d8f5f22
Aliaksei Syrel <alex.syrel@gmail.com>
2022-07-08 14:57:02 +0430

    add arm64 build

--- shared-library-builder/library/src/lib.rs
+++ shared-library-builder/library/src/lib.rs
@@ -1,9 +1,15 @@
 use shared_library_builder::{GitLocation, LibraryLocation, RustLibrary};
 
-pub fn libpixels(version: impl Into<String>) -> RustLibrary {
-    RustLibrary::new(
-        "Pixels",
-        LibraryLocation::Git(GitLocation::github("feenkcom", "libpixels").tag(version)),
-    )
-    .package("libpixels")
+pub fn libpixels(version: Option<impl Into<String>>) -> RustLibrary {
+    let mut location = GitLocation::github("feenkcom", "libpixels");
+    if let Some(version) = version {
+        location = location.tag(version);
+    }
+
+    RustLibrary::new("Pixels", LibraryLocation::Git(location)).package("libpixels")
+}
+
+pub fn latest_libpixels() -> RustLibrary {
+    let version: Option<String> = None;
+    libpixels(version)
 }
